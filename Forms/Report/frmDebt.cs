27635250d62a1fc6d0ad583a358a76648d99783d
Hauja47 <hoangletrunghau01@gmail.com>
2021-07-01 03:34:32 +0700

    Update UI and add logic

--- Forms/Report/frmDebt.cs
+++ Forms/Report/frmDebt.cs
@@ -16,5 +16,108 @@ namespace BookStore.Forms.Report
         {
             InitializeComponent();
         }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            //var reportsInMonth = BUS.DebtReport.Instance.Reports.Where(report =>
+            //                    report.Month == (int)nudMonth.Value && 
+            //                    report.Year == (int)nudPublishingYear.Value);
+
+            //if (reportsInMonth.Count() != 0)
+            //{
+            //    return;
+            //}
+
+            //CreateNew(reportsInMonth);
+
+            gunaDgvAccount.DataSource = BUS.DebtReport.Instance.Reports.Where(reprt =>
+                reprt.Month == (int)nudMonth.Value &&
+                reprt.Year == (int)nudPublishingYear.Value
+            ).ToList();
+        }
+
+        private void frmDebt_Load(object sender, EventArgs e)
+        {
+            //nudMonth.Maximum = DateTime.Now.Month != 1 ?
+            //    DateTime.Now.Month - 1 : 12;
+            //nudPublishingYear.Maximum = nudMonth.Maximum == 12 ?
+            //    DateTime.Now.Year - 1 : DateTime.Now.Year;
+
+            gunaDgvCustomer.DataSource = BUS.Customer.Instance.Customers;
+        }
+
+        #region Search
+        private void gunaTxtSearch_TextChanged(object sender, EventArgs e)
+        {
+            this.AcceptButton = btnSearch;
+
+            if (string.IsNullOrEmpty(gunaTxtSearch.Text))
+            {
+                gunaDgvCustomer.DataSource = BUS.Customer.Instance.Customers;
+                return;
+            }
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(gunaTxtSearch.Text))
+            {
+                gunaDgvCustomer.DataSource = BUS.Customer.Instance.Customers;
+                return;
+            }
+
+            string searchText = gunaTxtSearch.Text;
+
+            searchText = MyConvert.ToAscii(searchText).ToLower();
+
+            gunaDgvCustomer.DataSource =
+                BUS.Customer.Instance.Customers.Where(cus =>
+                {
+                    string customerName = MyConvert.ToAscii(cus.Name).ToLower();
+                    string address = MyConvert.ToAscii(cus.Address).ToLower();
+                    string phone = MyConvert.ToAscii(cus.PhoneNumber).ToLower();
+                    string email = MyConvert.ToAscii(cus.Email).ToLower();
+
+                    return customerName.Contains(searchText) || address.Contains(searchText)
+                        || phone.Contains(searchText) || email.Contains(searchText);
+                }).ToList();
+        }
+
+        private void gunaTxtSearch_Leave(object sender, EventArgs e)
+        {
+            this.AcceptButton = null;
+        }
+        #endregion
+
+        private void gunaDgvAccount_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnCancelFilter.Enabled = true;
+
+            gunaDgvCustomer.DataSource = BUS.Customer.Instance.Customers.Where(customer =>
+                customer.Id == (int)gunaDgvAccount.SelectedRows[0].Cells[0].Value
+            ).ToList();
+        }
+
+        private void gunaDgvCustomer_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            btnCancelFilter.Enabled = true;
+
+            gunaDgvAccount.DataSource = BUS.DebtReport.Instance.Reports.Where(report =>
+                report.CustomerId == (int)gunaDgvCustomer.SelectedRows[0].Cells[0].Value
+                && report.Month == (int)nudMonth.Value
+                && report.Year == (int)nudPublishingYear.Value
+            ).ToList();
+        }
+
+        private void btnCancelFilter_Click(object sender, EventArgs e)
+        {
+            btnCancelFilter.Enabled = false;
+
+            gunaDgvCustomer.DataSource = BUS.Customer.Instance.Customers;
+            gunaDgvAccount.DataSource = BUS.DebtReport.Instance.Reports.Where(reprt =>
+                reprt.Month == (int)nudMonth.Value &&
+                reprt.Year == (int)nudPublishingYear.Value
+            ).ToList();
+        }
     }
 }
