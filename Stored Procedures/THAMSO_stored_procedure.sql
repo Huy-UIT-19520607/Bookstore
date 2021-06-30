0f0bbb7b1d0971b5dc28e9ca759ee117fe418a18
Huy-UIT-19520607 <19520607@gm.uit.edu.vn>
2021-06-30 14:52:46 +0700

    Update THAMSO_stored_procedure.sql

--- Stored Procedures/THAMSO_stored_procedure.sql
+++ Stored Procedures/THAMSO_stored_procedure.sql
@@ -7,12 +7,18 @@ create proc sp_change_value_parameter
 @name nvarchar(200),
 @value int
 as
+begin
+	if @name=N'TiLeTinhDonGiaBan'
+	begin
+		declare @old_value int
+		select @old_value = GiaTri from THAMSO where TenThamSo=N'TiLeTinhDonGiaBan'
+		update SACH
+		set
+			DonGiaBan=(DonGiaBan*100/@old_value)*@value/100
+	end
+
 	update THAMSO
 	set GiaTri=@value
 	where TenThamSo=@name
-
-	if @name=N'TiLeTinhDonGiaBan'
-		update SACH
-		set
-			DonGiaBan=DonGiaBan*@value/100
+end
 go
